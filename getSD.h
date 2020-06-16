9278e9bfc1b16313cc610a0182cce26c036892fc
goredoc <goredoc@gmail.com>
2020-06-16 19:05:36 -0400

    Add files via upload

---
+++ getSD.h
@@ -0,0 +1,32 @@
+double getSDftn (vector<double> xVector) {
+    double sumX; // sum of x values
+    sumX = 0.0;
+    
+    double sumX2; // sum of x squared values
+    sumX2 = 0.0;
+
+    double squaredSumX; // squared sum of x
+    squaredSumX = 0.0;
+
+    double variance; // with n in denominator
+    variance = 0.0;
+
+    double sd; // standard deviation
+    sd = 0.0;
+
+    int vectorLength;
+    vectorLength = xVector.size();
+
+    for (int i=0; i < vectorLength ; ++i)
+    {
+        sumX  += xVector[i];
+        sumX2 += xVector[i]*xVector[i];
+    }
+
+    squaredSumX = sumX * sumX;
+
+    variance = (sumX2 - squaredSumX/vectorLength)/vectorLength;
+    sd = sqrt(variance);
+
+    return(sd);
+}
